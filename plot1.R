9ecb7a88a656cb5e146857df083b2bd7b11072b8
DawitHabtemariam <dhabtema@haverford.edu>
2014-08-06 11:06:45 -0400

    new

--- plot1.R
+++ plot1.R
@@ -1,46 +1,13 @@
-YWSID <- "..."
-CONSUMER_KEY <- "..."
-CONSUMER_SECRET <- "..."
-TOKEN <- "..."
-TOKEN_SECRET <- "..."
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","power.csv")
+unzip("power.csv")
+power<-read.table("household_power_consumption.txt",sep=";",header=T,stringsAsFactors=F)
 
-signature <- as.character(curlPercentEncode(base64(hmac(key=paste(cons_sec, tok_sec, sep="&"), object=api_url, algo="sha1", serialize=FALSE, raw=TRUE))))
+data<-subset(power,Date=="1/2/2007" | Date=="2/2/2007")
 
-yelp_bus <- function(rest, ywsid, cons_key, cons_sec, tok, tok_sec) {
-  require(package = "digest")
-  require(package = "rjson")
-  require(package = "RCurl")
-  require(package = "stringr")
-  rest1 <- gsub(pattern = " ", replacement = "%2520", x = rest)
-  rest2 <- gsub(pattern = " ", replacement = "+", x = rest)
-  nonce <- paste(sample(x = c(0:9, letters, LETTERS, "_"), size = 32, replace = TRUE), collapse = "")
-  tm <- format(x = Sys.time(), "%s")
-  api_url <- paste("GET\u0026http%3A%2F%2Fapi.yelp.com%2Fv2%2Fsearch\u0026",
-                   "limit%3D1%26location%3DChicago%2520IL%26",
-                   "oauth_consumer_key%3D", cons_key, "%26",
-                   "oauth_nonce%3D", nonce, "%26",
-                   "oauth_signature_method%3DHMAC-SHA1%26",
-                   "oauth_timestamp%3D", tm, "%26",
-                   "oauth_token%3D", tok, "%26",
-                   "term%3D", rest1,
-                   sep = "")
-  signature <- hmac(key = tok_sec, object = api_url, algo = "sha1")
-  api_url <- paste("http://api.yelp.com/v2/search?",
-                   "limit=1&location=Chicago+IL&",
-                   "oauth_consumer_key=", cons_key, "&",
-                   "oauth_nonce=", nonce, "&",
-                   "oauth_signature=", signature, "&",
-                   "oauth_signature_method=HMAC-SHA1&",
-                   "oauth_timestamp=", tm, "&",
-                   "oauth_token=", tok, "&",
-                   "term=", rest2,
-                   sep = "" )
-  return(api_url)
-}
+data$Date <- strptime(paste(data$Date,data$Time), "%d/%m/%Y %H:%M:%S")
 
-yelp_bus(rest = "chez moi",
-         ywsid = YWSID,
-         cons_key = CONSUMER_KEY,
-         cons_sec = CONSUMER_SECRET,
-         tok = TOKEN,
-         tok_sec = TOKEN_SECRET)
+hist(as.numeric(data$Global_active_power),col="red",xlab="Global Active Power (in kilowatts)", main="Global Active Power")
+
+dev.copy(png,"plot1.png")
+
+dev.off()
